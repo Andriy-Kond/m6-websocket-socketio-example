6c71a9e7e9e2bcbf1b891a49f3a9e5859bac8bdc
Andrii <andrii.kondratovich@gmail.com>
2025-01-24 15:24:35 +0200

    INIT frontend

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,45 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/query";
-
-import {
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-
-import { persistedUserAuthReducer } from "features/auth/authSlice";
-
-import { contactsApi } from "features/contacts/contactsSlice";
-import { filtersSlice } from "features/filters/filtersSlice";
-import { usersApi } from "features/users/usersSlice";
-
-import { tasksApi } from "features/tasks/tasksSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: persistedUserAuthReducer,
-    [usersApi.reducerPath]: usersApi.reducer,
-    [contactsApi.reducerPath]: contactsApi.reducer,
-    filters: filtersSlice.reducer,
+    counter: counterReducer,
   },
-
-  middleware: getDefaultMiddleware => [
-    ...getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-    contactsApi.middleware,
-    usersApi.middleware,
-
-    tasksApi.middleware,
-  ],
 });
-
-export const persistor = persistStore(store);
-
-setupListeners(store.dispatch); // used to enable refetchOnMount and refetchOnReconnect behaviors.
